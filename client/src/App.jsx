36ef928cdd812837bfaca38648fa6af348d7a9f4
ocaso1323 <ap3717402@gmail.com>
2023-11-08 22:12:59 -0500

    subiendo

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bienvenido!!</h1>
+      <div>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+    </div>
+    </div>
+    
+  );
+}
